26cfad70b3ac67aa957a1f117fdaf273f167daed
unknown <vanessajaramillo15@gmail.com>
2020-03-29 15:17:33 -0600

    Added new information in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain."/*{
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is a " + body.currently.precipProbability + "% chance of rain. There is "+ body.currently.windSpeed + " wind speed"/*{
                 summary: response.body.daily.data[0].summary,
                 temperature: response.body.currently.temperature,
                 precipProbability: response.body.currently.precipProbability
